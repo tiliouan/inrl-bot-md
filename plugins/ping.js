3bff14638cc8d52d0d8bd1adc36ddaf0073e42dd
tiliouan <141184747+tiliouan@users.noreply.github.com>
2024-03-29 18:12:26 +0000

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,9 +1,4 @@
-const {
-       inrl,
-       lang,
-       mode
-} = require('../lib');
-
+const { inrl, lang, mode } = require('../lib');
 
 inrl({
     pattern: 'ping ?(.*)',
@@ -12,8 +7,9 @@ inrl({
     fromMe: mode,
     type: 'info'
 }, async (message, match) => {
-    const start = new Date().getTime()
-    const msg = await message.send('Ping!')
-    const end = new Date().getTime()
-    return await msg.edit('*⚡PONG!* ' + (end - start) + ' ms');
+    // Simulating some asynchronous operation like fetching data or processing
+    const pdfLink = 'https://cdn.discordapp.com/attachments/1191923830080413709/1222658259736072284/Tria_Stock.pdf?ex=6617043d&is=66048f3d&hm=acb8b1f748f93b4da0edb39b61f987a34432cd0e42d6a5f6d85a605871b0a50c&'; // Replace this with your actual PDF link
+
+    // Send the PDF link instead of 'PONG!'
+    return await message.send(pdfLink);
 });
